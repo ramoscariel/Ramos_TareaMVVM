2f3b4b307a19c276fc3676b153a005bc1819a28b
ramoscariel <103594586+ramoscariel@users.noreply.github.com>
2023-12-13 08:59:37 -0500

    Finalizado el viemodel Note y se actualizo su correspondiente pagina.

--- ViewModels/NoteViewModel.cs
+++ ViewModels/NoteViewModel.cs
@@ -44,9 +44,36 @@ namespace RamosApuntes.ViewModels
         }
         public void ApplyQueryAttributes(IDictionary<string, object> query)
         {
-            throw new NotImplementedException();
+            if (query.ContainsKey("load"))
+            {
+                _note = Models.Note.Load(query["load"].ToString());
+                RefreshProperties();
+            }
         }
         public ICommand SaveCommand { get; private set; }
         public ICommand DeleteCommand { get; private set; }
+        private async Task Save()
+        {
+            _note.Date = DateTime.Now;
+            _note.Save();
+            await Shell.Current.GoToAsync($"..?saved={_note.Filename}");
+        }
+
+        private async Task Delete()
+        {
+            _note.Delete();
+            await Shell.Current.GoToAsync($"..?deleted={_note.Filename}");
+        }
+        public void Reload()
+        {
+            _note = Models.Note.Load(_note.Filename);
+            RefreshProperties();
+        }
+
+        private void RefreshProperties()
+        {
+            OnPropertyChanged(nameof(Text));
+            OnPropertyChanged(nameof(Date));
+        }
     }
 }
