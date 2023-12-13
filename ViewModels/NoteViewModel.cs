a887b80119a49d5006b834fa7219d35c49bdb52e
ramoscariel <103594586+ramoscariel@users.noreply.github.com>
2023-12-13 08:43:11 -0500

    Creacion de View Models y actualizacionn de AboutPage.

---
+++ ViewModels/NoteViewModel.cs
@@ -0,0 +1,52 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace RamosApuntes.ViewModels
+{
+
+    internal class NoteViewModel : ObservableObject, IQueryAttributable
+    {
+        public string Text
+        {
+            get => _note.Text;
+            set
+            {
+                if (_note.Text != value)
+                {
+                    _note.Text = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public DateTime Date => _note.Date;
+
+        public string Identifier => _note.Filename;
+        private Models.Note _note;
+        public NoteViewModel()
+        {
+            _note = new Models.Note();
+            SaveCommand = new AsyncRelayCommand(Save);
+            DeleteCommand = new AsyncRelayCommand(Delete);
+        }
+
+        public NoteViewModel(Models.Note note)
+        {
+            _note = note;
+            SaveCommand = new AsyncRelayCommand(Save);
+            DeleteCommand = new AsyncRelayCommand(Delete);
+        }
+        public void ApplyQueryAttributes(IDictionary<string, object> query)
+        {
+            throw new NotImplementedException();
+        }
+        public ICommand SaveCommand { get; private set; }
+        public ICommand DeleteCommand { get; private set; }
+    }
+}
--- Views/AboutPage.xaml.cs
+++ Views/AboutPage.xaml.cs
@@ -9,10 +9,10 @@ public partial class AboutPage : ContentPage
     private async void LearnMore_Clicked(object sender, EventArgs e)
     {
 
-        if (BindingContext is Models.About about)
+        /*if (BindingContext is Models.About about)
         {
             // Navigate to the specified URL in the system browser.
             await Launcher.Default.OpenAsync(about.MoreInfoUrl);
-        }
+        }*/
     }
 }
